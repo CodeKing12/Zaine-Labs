27278efea9e93bdc84285c3f9e2ea6e9e543cbdc
Kingsley <eyetukingsley330@gmail.com>
2022-05-03 01:35:23 +0100

    I am done with the table.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ["./sources/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // screens: {
+      //   'xxs': '350px',
+      //   'xs': '520px',
+      //   ...defaultTheme.screens,
+      // }
+    },
+  },
+  plugins: [],
+}
